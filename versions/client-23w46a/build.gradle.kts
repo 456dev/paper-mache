20947dcf5ce871cc8969c51936d2cae0e9dd9681
The456gamer <the456gamer@the456gamer.dev>
2023-11-23 01:18:47 +0000

    client 23w46a

---
+++ versions/client-23w46a/build.gradle.kts
@@ -0,0 +1,25 @@
+import io.papermc.mache.util.MinecraftSide
+
+plugins {
+    id("mache")
+}
+
+mache {
+    minecraftVersion = "23w46a"
+    minecraftJarType = MinecraftSide.CLIENT
+    repositories.register("sonatype snapshots") {
+        url.set("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        includeGroups.set(listOf("org.vineflower"))
+    }
+}
+
+dependencies {
+    codebook("1.0.7")
+    remapper(art("1.0.7"))
+    decompiler(vineflower("1.10.0-SNAPSHOT"))
+    parchment("1.20.1", "2023.09.03")
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+}
