68fc7a1a91b9db4bb055b78fb047350cb2df9110
Isaac - The456 <the456@the456gamer.dev>
2023-12-19 23:25:31 +0000

    migrate to client 1.20.4

--- versions/client-1.20.4/build.gradle.kts
+++ versions/client-1.20.4/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 mache {
-    minecraftVersion = "1.20.3"
+    minecraftVersion = "1.20.4"
     minecraftJarType = MinecraftSide.CLIENT
     repositories.register("sonatype snapshots") {
         url.set("https://s01.oss.sonatype.org/content/repositories/snapshots/")
